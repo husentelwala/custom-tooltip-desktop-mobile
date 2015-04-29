e55aaefe5b158dcf6ddcff3c515a294b24f5ac19
husentelwala <husen.telwala@hiddenbrains.in>
2015-04-29 14:05:05 +0530

    Required JS Changes for tooltip

--- js/customize-tooltip.js
+++ js/customize-tooltip.js
@@ -4,66 +4,72 @@
 /***** VERSION 2 : 09-10-2014
 /*************************/
 $(document).ready(function(){
-	$windowwidth=$(window).width();
+	
 	$cust_tooltip=$('.custom-tooltip');
 	$cust_tooltip.wrap('<div class="custom-tooltip-parent"></div>');
-	$('.custom-tooltip-parent').each(function(){
-		$(this).append('<p class="custom-tooltip-msg"></p>');
-		$(this).find('.custom-tooltip-msg').text($(this).find($cust_tooltip).attr('title'));
-		$vertical_space_ttip_content=6;
-		$horizontal_space_ttip_content=10;
-		
-		$a_element_height_tooltip=$(this).find('.custom-tooltip').outerHeight();
-		$a_element_width_tooltip=$(this).find('.custom-tooltip').outerWidth();
-		$p_element_height_tooltip=$(this).find('.custom-tooltip-msg').outerHeight();
-		$p_element_width_tooltip=$(this).find('.custom-tooltip-msg').outerWidth();
-		$data_placement_tooltip= $(this).find($cust_tooltip).attr('data-placement');
-
-		if($data_placement_tooltip==='top')
-		{
-			fn_tooltip_top(this);
-		}
-		else if($data_placement_tooltip==='bottom')
-		{
-			fn_tooltip_bottom(this);
-		}
-		else if($data_placement_tooltip==='left')
-		{
-			fn_tooltip_left(this);
-		}
-		else if($data_placement_tooltip==='right')
-		{
-			fn_tooltip_right(this);
-		}
-		else if($data_placement_tooltip==='right-desktop-left-mobile')
-		{
-			if($windowwidth>768) { fn_tooltip_left(this); } else { fn_tooltip_right(this); }
-		}		
-		else if($data_placement_tooltip==='left-desktop-right-mobile')
-		{
-			if($windowwidth>768) { fn_tooltip_right(this); } else { fn_tooltip_left(this); }
-		}
-		else if($data_placement_tooltip==='right-desktop-top-mobile')
-		{
-			if($windowwidth>768) { fn_tooltip_left(this); } else { fn_tooltip_top(this); }
-		}		
-		else if($data_placement_tooltip==='left-desktop-top-mobile')
-		{
-			if($windowwidth>768) { fn_tooltip_right(this); } else { fn_tooltip_top(this); }
-		}
-		else if($data_placement_tooltip==='right-desktop-bottom-mobile')
-		{
-			if($windowwidth>768) { fn_tooltip_left(this); } else { fn_tooltip_bottom(this); }
-		}		
-		else if($data_placement_tooltip==='left-desktop-bottom-mobile')
-		{
-			if($windowwidth>768) { fn_tooltip_right(this); } else { fn_tooltip_bottom(this); }
-		}
-		$(this).find('p').css({
-			top : $p_top_position_tooltip,
-			left : $p_left_position_tooltip
+	condtionalTooltipAlignment();
+	function condtionalTooltipAlignment() {
+		$windowwidth=$(window).width();
+		$('.custom-tooltip-parent').each(function(){
+			$(this).append('<p class="custom-tooltip-msg"></p>');
+			$(this).find('.custom-tooltip-msg').text($(this).find($cust_tooltip).attr('title'));
+			$vertical_space_ttip_content=6;
+			$horizontal_space_ttip_content=10;
+			
+			$a_element_height_tooltip=$(this).find('.custom-tooltip').outerHeight();
+			$a_element_width_tooltip=$(this).find('.custom-tooltip').outerWidth();
+			$p_element_height_tooltip=$(this).find('.custom-tooltip-msg').outerHeight();
+			$p_element_width_tooltip=$(this).find('.custom-tooltip-msg').outerWidth();
+			$data_placement_tooltip= $(this).find($cust_tooltip).attr('data-placement');
+	
+			
+			
+			if($data_placement_tooltip==='top')
+			{
+				fn_tooltip_top(this);
+			}
+			else if($data_placement_tooltip==='bottom')
+			{
+				fn_tooltip_bottom(this);
+			}
+			else if($data_placement_tooltip==='left')
+			{
+				fn_tooltip_left(this);
+			}
+			else if($data_placement_tooltip==='right')
+			{
+				fn_tooltip_right(this);
+			}
+			else if($data_placement_tooltip==='right-desktop-left-mobile')
+			{
+				if($windowwidth>768) { fn_tooltip_left(this); } else { fn_tooltip_right(this); }
+			}		
+			else if($data_placement_tooltip==='left-desktop-right-mobile')
+			{
+				if($windowwidth>768) { fn_tooltip_right(this); } else { fn_tooltip_left(this); }
+			}
+			else if($data_placement_tooltip==='right-desktop-top-mobile')
+			{
+				if($windowwidth>768) { fn_tooltip_left(this); } else { fn_tooltip_top(this); }
+			}		
+			else if($data_placement_tooltip==='left-desktop-top-mobile')
+			{
+				if($windowwidth>768) { fn_tooltip_right(this); } else { fn_tooltip_top(this); }
+			}
+			else if($data_placement_tooltip==='right-desktop-bottom-mobile')
+			{
+				if($windowwidth>768) { fn_tooltip_left(this); } else { fn_tooltip_bottom(this); }
+			}		
+			else if($data_placement_tooltip==='left-desktop-bottom-mobile')
+			{
+				if($windowwidth>768) { fn_tooltip_right(this); } else { fn_tooltip_bottom(this); }
+			}
+			$(this).find('p').css({
+				top : $p_top_position_tooltip,
+				left : $p_left_position_tooltip
+			});
 		});
-	});
+	}
 	function fn_tooltip_top(obj)
 	{
 		$(obj).addClass('top');
@@ -88,4 +94,23 @@ $(document).ready(function(){
 		$p_top_position_tooltip=-(($p_element_height_tooltip-$a_element_height_tooltip)/2);
 		$p_left_position_tooltip=-($p_element_width_tooltip + $horizontal_space_ttip_content);
 	}
+	
+	/*
+	 * WINDOW WIDTH RESIZE EVENT HANDLER ***********************/
+	function checkWindowWidth(callback){
+		var lastHeight = $(window).width(), newHeight, timer;
+		(function run(){
+			newHeight = $(window).width();
+			if( lastHeight != newHeight )
+				callback();
+			lastHeight = newHeight;
+			timer = setTimeout(run, 200);	
+		})();
+	}
+	
+	function windowWidthChanged(){
+	  condtionalTooltipAlignment();
+	}
+	checkWindowWidth(windowWidthChanged);
+	/* ENDED ***********************/
 });
